dd9b3aff4e5960eab3520a50af843d5f950f14a6
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-21 23:44:36 +0200

    fix: All. The. Bugs... in the data module
    a.k.a: I forgot variables don't get hoisted like
    functions, and React uses camelcase even for
    attributes that are a single word in HTML. *The
    more you know*

--- src/form-data.js
+++ src/form-data.js
@@ -1,9 +1,3 @@
-export const formData = [personalData, educationData, workData];
-export const inputTemplates = {
-    education: educationInputsTemplate,
-    work: workInputsTemplate,
-};
-
 const CURRENT_YEAR = new Date().getFullYear();
 
 const personalData = {
@@ -17,7 +11,7 @@ const personalData = {
                     label: "Name",
                     type: "text",
                     placeholder: "ex. 'John Doe'",
-                    inputProps: { autocapitalize: "words" },
+                    inputProps: { autoCapitalize: "words" },
                 },
                 {
                     label: "Email",
@@ -28,18 +22,81 @@ const personalData = {
                     label: "Phone number",
                     type: "tel",
                     placeholder: "ex. '+44 444 444 44'",
-                    inputProps: { maxlength: 20 },
+                    inputProps: { maxLength: 20 },
                 },
                 {
                     label: "Location",
                     type: "text",
                     placeholder: "ex. 'Boring, Oregon'",
-                    inputProps: { autocapitalize: "words" },
+                    inputProps: { autoCapitalize: "words" },
                 },
             ],
         },
     ],
 };
+
+const educationInputsTemplate = [
+    {
+        label: "School name",
+        type: "text",
+        placeholder: "ex. 'Monsters University (MU)'",
+        inputProps: { autoCapitalize: "words" },
+    },
+    {
+        label: "Degree name",
+        type: "text",
+        placeholder: "ex. 'BA in Scares'",
+        inputProps: { autoCapitalize: "words" },
+    },
+    {
+        label: "From:",
+        type: "number",
+        inputProps: { min: 1900, max: CURRENT_YEAR },
+    },
+    {
+        label: "To:",
+        type: "number",
+        inputProps: { min: 1900, max: CURRENT_YEAR },
+    },
+    {
+        label: "Relevant courses",
+        type: "text",
+        placeholder: "example:\n-Scares 101\n-Advanced sneaking",
+        inputProps: { autoCapitalize: "sentence" },
+    },
+];
+
+const workInputsTemplate = [
+    {
+        label: "Company name",
+        type: "text",
+        placeholder: "ex. 'Monsters Inc.'",
+        inputProps: { autoCapitalize: "words" },
+    },
+    {
+        label: "Job title",
+        type: "text",
+        placeholder: "ex. 'Scare floor supervisor'",
+        inputProps: { autoCapitalize: "words" },
+    },
+    {
+        label: "From:",
+        type: "number",
+        inputProps: { min: 1900, max: CURRENT_YEAR },
+    },
+    {
+        label: "To:",
+        type: "number",
+        inputProps: { min: 1900, max: CURRENT_YEAR },
+    },
+    {
+        label: "Responsibilities",
+        type: "text",
+        placeholder:
+            "example:\n-Coordinating scare teams\n-Creating performance reports",
+        inputProps: { autoCapitalize: "sentence" },
+    },
+];
 
 const educationData = {
     id: "education",
@@ -63,65 +120,8 @@ const workData = {
     ],
 };
 
-const educationInputsTemplate = [
-    {
-        label: "School name",
-        type: "text",
-        placeholder: "ex. 'Monsters University (MU)'",
-        inputProps: { autocapitalize: "words" },
-    },
-    {
-        label: "Degree name",
-        type: "text",
-        placeholder: "ex. 'BA in Scares'",
-        inputProps: { autocapitalize: "words" },
-    },
-    {
-        label: "From:",
-        type: "number",
-        inputProps: { min: 1900, max: CURRENT_YEAR },
-    },
-    {
-        label: "To:",
-        type: "number",
-        inputProps: { min: 1900, max: CURRENT_YEAR },
-    },
-    {
-        label: "Relevant courses",
-        type: "text",
-        placeholder: "example:\n-Scares 101\n-Advanced sneaking",
-        inputProps: { autocapitalize: "sentence" },
-    },
-];
-
-const workInputsTemplate = [
-    {
-        label: "Company name",
-        type: "text",
-        placeholder: "ex. 'Monsters Inc.'",
-        inputProps: { autocapitalize: "words" },
-    },
-    {
-        label: "Job title",
-        type: "text",
-        placeholder: "ex. 'Scare floor supervisor'",
-        inputProps: { autocapitalize: "words" },
-    },
-    {
-        label: "From:",
-        type: "number",
-        inputProps: { min: 1900, max: CURRENT_YEAR },
-    },
-    {
-        label: "To:",
-        type: "number",
-        inputProps: { min: 1900, max: CURRENT_YEAR },
-    },
-    {
-        label: "Responsibilities",
-        type: "text",
-        placeholder:
-            "example:\n-Coordinating scare teams\n-Creating performance reports",
-        inputProps: { autocapitalize: "sentence" },
-    },
-];
+export const formData = [personalData, educationData, workData];
+export const inputTemplates = {
+    education: educationInputsTemplate,
+    work: workInputsTemplate,
+};
