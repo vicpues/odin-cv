6a85218051cc309f02b31825c850c1379253938b
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-21 19:47:46 +0200

    feat: Update Input component with new props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,26 @@
-export default function Input({ label, icon = null, onChange, inputProps }) {
+export default function Input({
+  label,
+  type,
+  placeholder = "",
+  value = "",
+  icon = null,
+  onChange,
+  inputProps,
+}) {
   if (icon !== null) icon.className = "input-icon";
 
   return (
     <label className="input-wrapper">
       <span className="input-label">{label}</span>
       {icon}
-      <input onChange={onChange} {...inputProps} className="input-field" />
+      <input
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        {...inputProps}
+        className="input-field"
+      />
     </label>
   );
 }
