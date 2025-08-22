5446111807069d54e5178860987feca191c6b336
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-22 02:31:41 +0200

    feat: Add work form section

--- src/components/sections/Form/Form.jsx
+++ src/components/sections/Form/Form.jsx
@@ -1,11 +1,13 @@
 import PersonalSection from "./PersonalSection/PersonalSection";
 import EducationSection from "./EducationSection/EducationSection";
+import WorkSection from "./WorkSection/WorkSection";
 
 export default function Form() {
   return (
     <form id="cv-form" action="">
       <PersonalSection />
       <EducationSection />
+      <WorkSection />
     </form>
   );
 }
---
+++ src/components/sections/Form/WorkSection/WorkSection.jsx
@@ -0,0 +1,48 @@
+import FormSection from "../FormSection/FormSection";
+import Input from "../Input/Input";
+import InputGroup from "../InputGroup/InputGroup";
+
+export default function WorkSection() {
+  const CURRENT_YEAR = new Date().getFullYear();
+
+  return (
+    <FormSection id="work-details" legend="Work experience">
+      <InputGroup>
+        <Input
+          label="Company name"
+          type="text"
+          placeholder="ex. 'Monsters Inc.'"
+          inputProps={{ autoCapitalize: "words" }}
+        />
+
+        <Input
+          label="Job title"
+          type="text"
+          placeholder="ex. 'Scare floor supervisor'"
+          inputProps={{ autoCapitalize: "words" }}
+        />
+
+        <Input
+          label="From:"
+          type="number"
+          inputProps={{ min: 1900, max: CURRENT_YEAR }}
+        />
+
+        <Input
+          label="To:"
+          type="number"
+          inputProps={{ min: 1900, max: CURRENT_YEAR }}
+        />
+
+        <Input
+          label="Responsibilities"
+          type="text"
+          placeholder={
+            "example:\n-Coordinating scare teams\n-Creating performance reports"
+          }
+          inputProps={{ autoCapitalize: "sentence" }}
+        />
+      </InputGroup>
+    </FormSection>
+  );
+}
