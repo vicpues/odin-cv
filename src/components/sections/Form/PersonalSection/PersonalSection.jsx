bf5bd004b591369cc42906f58c41e5841512f968
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-22 02:15:24 +0200

    fix: Use void elements in personal section

--- src/components/sections/Form/PersonalSection/PersonalSection.jsx
+++ src/components/sections/Form/PersonalSection/PersonalSection.jsx
@@ -9,27 +9,23 @@ export default function PersonalSection() {
         type="text"
         placeholder="ex. 'John Doe'"
         inputProps={{ autoCapitalize: "words" }}
-      ></Input>
+      />
 
-      <Input
-        label="Email"
-        type="text"
-        placeholder="ex. 'email@example.com"
-      ></Input>
+      <Input label="Email" type="text" placeholder="ex. 'email@example.com" />
 
       <Input
         label="Phone number"
         type="tel"
         placeholder="ex. '+44 444 444 44'"
         inputProps={{ maxLength: 20 }}
-      ></Input>
+      />
 
       <Input
         label="Location"
         type="text"
         placeholder="ex. 'Boring, Oregon'"
         inputProps={{ autoCapitalize: "words" }}
-      ></Input>
+      />
     </FormSection>
   );
 }
