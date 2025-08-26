411967ed6baf08218997c26a1ecc5dc5dc172446
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-26 23:15:08 +0200

    feat: Add state updates to education section

--- src/components/sections/Form/EducationSection.jsx
+++ src/components/sections/Form/EducationSection.jsx
@@ -1,51 +1,79 @@
 import Input from "./Input/Input";
 import InputGroup from "./InputGroup/InputGroup";
 import FormSection from "./FormSection/FormSection";
+import getFormData from "./InputGroup/getFormData";
 
-export default function EducationSection({ data }) {
-  const CURRENT_YEAR = new Date().getFullYear();
-
+export default function EducationSection({ data, updateData }) {
   return (
-    <FormSection id="education-details" title="Education">
-      <InputGroup data={data}>
-        <Input
-          label="School name"
-          name="school"
-          type="text"
-          placeholder="ex. 'Monsters University (MU)'"
-          inputProps={{ autoCapitalize: "words" }}
+    <FormSection title="Education">
+      {data.educationData.map((groupData) => (
+        <GroupTemplate
+          key={groupData.uniqueId}
+          groupData={groupData}
+          onUpdate={(e) => {
+            e.preventDefault();
+            const index = data.educationData.findIndex(
+              (group) => group.uniqueId === groupData.uniqueId,
+            );
+            const newData = [...data.educationData];
+            newData[index] = {
+              ...getFormData(e.target),
+              uniqueId: groupData.uniqueId,
+            };
+            updateData({ ...data, educationData: newData });
+          }}
         />
-
-        <Input
-          label="Degree name"
-          name="degree"
-          type="text"
-          placeholder="ex. 'BA in Scares'"
-          inputProps={{ autoCapitalize: "words" }}
-        />
-
-        <Input
-          label="From:"
-          name="from"
-          type="number"
-          inputProps={{ min: 1900, max: CURRENT_YEAR }}
-        />
-
-        <Input
-          label="To:"
-          name="to"
-          type="number"
-          inputProps={{ min: 1900, max: CURRENT_YEAR }}
-        />
-
-        <Input
-          label="Relevant courses"
-          name="details"
-          type="textarea"
-          placeholder={"example:\n-Scares 101\n-Advanced sneaking"}
-          inputProps={{ autoCapitalize: "sentence" }}
-        />
-      </InputGroup>
+      ))}
     </FormSection>
+  );
+}
+
+function GroupTemplate({ groupData, onUpdate }) {
+  const CURRENT_YEAR = new Date().getFullYear();
+  return (
+    <InputGroup onUpdate={onUpdate}>
+      <Input
+        label="School name"
+        name="school"
+        type="text"
+        placeholder="ex. 'Monsters University (MU)'"
+        defaultValue={groupData.school}
+        inputProps={{ autoCapitalize: "words" }}
+      />
+
+      <Input
+        label="Degree name"
+        name="degree"
+        type="text"
+        placeholder="ex. 'BA in Scares'"
+        defaultValue={groupData.degree}
+        inputProps={{ autoCapitalize: "words" }}
+      />
+
+      <Input
+        label="From:"
+        name="from"
+        type="number"
+        defaultValue={groupData.from}
+        inputProps={{ min: 1900, max: CURRENT_YEAR }}
+      />
+
+      <Input
+        label="To:"
+        name="to"
+        type="number"
+        defaultValue={groupData.to}
+        inputProps={{ min: 1900, max: CURRENT_YEAR }}
+      />
+
+      <Input
+        label="Relevant courses"
+        name="details"
+        type="textarea"
+        placeholder={"example:\n-Scares 101\n-Advanced sneaking"}
+        defaultValue={groupData.details}
+        inputProps={{ autoCapitalize: "sentence" }}
+      />
+    </InputGroup>
   );
 }
