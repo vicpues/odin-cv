a86252127ca7017e9ac40e7aebaa0b78ef45ec88
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-26 23:27:03 +0200

    feat: Add state updates to work section

--- src/components/sections/Form/WorkSection.jsx
+++ src/components/sections/Form/WorkSection.jsx
@@ -1,53 +1,81 @@
-import FormSection from "./FormSection/FormSection";
 import Input from "./Input/Input";
 import InputGroup from "./InputGroup/InputGroup";
+import FormSection from "./FormSection/FormSection";
+import getFormData from "./InputGroup/getFormData";
 
-export default function WorkSection({ data }) {
-  const CURRENT_YEAR = new Date().getFullYear();
-
+export default function WorkSection({ data, updateData }) {
   return (
     <FormSection id="work-details" title="Work experience">
-      <InputGroup data={data}>
-        <Input
-          label="Company name"
-          name="company"
-          type="text"
-          placeholder="ex. 'Monsters Inc.'"
-          inputProps={{ autoCapitalize: "words", autoComplete: "off" }}
+      {data.workData.map((groupData) => (
+        <GroupTemplate
+          key={groupData.uniqueId}
+          groupData={groupData}
+          onUpdate={(e) => {
+            e.preventDefault();
+            const index = data.workData.findIndex(
+              (group) => group.uniqueId === groupData.uniqueId,
+            );
+            const newData = [...data.workData];
+            newData[index] = {
+              ...getFormData(e.target),
+              uniqueId: groupData.uniqueId,
+            };
+            updateData({ ...data, workData: newData });
+          }}
         />
-
-        <Input
-          label="Job title"
-          name="position"
-          type="text"
-          placeholder="ex. 'Scare floor supervisor'"
-          inputProps={{ autoCapitalize: "words" }}
-        />
-
-        <Input
-          label="From:"
-          name="from"
-          type="number"
-          inputProps={{ min: 1900, max: CURRENT_YEAR }}
-        />
-
-        <Input
-          label="To:"
-          name="to"
-          type="number"
-          inputProps={{ min: 1900, max: CURRENT_YEAR }}
-        />
-
-        <Input
-          label="Responsibilities"
-          name="details"
-          type="textarea"
-          placeholder={
-            "example:\n-Coordinating scare teams\n-Creating performance reports"
-          }
-          inputProps={{ autoCapitalize: "sentence" }}
-        />
-      </InputGroup>
+      ))}
     </FormSection>
+  );
+}
+
+function GroupTemplate({ groupData, onUpdate }) {
+  const CURRENT_YEAR = new Date().getFullYear();
+  return (
+    <InputGroup onUpdate={onUpdate}>
+      <Input
+        label="Company name"
+        name="company"
+        type="text"
+        placeholder="ex. 'Monsters Inc.'"
+        defaultValue={groupData.company}
+        inputProps={{ autoCapitalize: "words", autoComplete: "off" }}
+      />
+
+      <Input
+        label="Job title"
+        name="position"
+        type="text"
+        placeholder="ex. 'Scare floor supervisor'"
+        defaultValue={groupData.position}
+        inputProps={{ autoCapitalize: "words" }}
+      />
+
+      <Input
+        label="From:"
+        name="from"
+        type="number"
+        defaultValue={groupData.from}
+        inputProps={{ min: 1900, max: CURRENT_YEAR }}
+      />
+
+      <Input
+        label="To:"
+        name="to"
+        type="number"
+        defaultValue={groupData.to}
+        inputProps={{ min: 1900, max: CURRENT_YEAR }}
+      />
+
+      <Input
+        label="Responsibilities"
+        name="details"
+        type="textarea"
+        placeholder={
+          "example:\n-Coordinating scare teams\n-Creating performance reports"
+        }
+        defaultValue={groupData.details}
+        inputProps={{ autoCapitalize: "sentence" }}
+      />
+    </InputGroup>
   );
 }
