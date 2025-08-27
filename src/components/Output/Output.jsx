97c1284cc722700e4d6ae6a93f7350c2f3abe80b
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-27 17:08:32 +0200

    fix: Do not render section if it is empty

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -14,38 +14,44 @@ export default function Output({ data }) {
         <span>{personal.location}</span>
       </div>
 
-      <hr />
-
-      <div>
-        <div>Education</div>
-        {education.map((group) => (
-          <div key={group.uniqueId}>
-            <div>{group.degree}</div>
-            <span>{group.school}</span>{" "}
-            <span>{getDateString(group.from, group.to)}</span>
-            <div>
-              <Paragraphs string={group.details} />
-            </div>
+      {education.length > 0 && (
+        <>
+          <hr />
+          <div>
+            <div>Education</div>
+            {education.map((group) => (
+              <div key={group.uniqueId}>
+                <div>{group.degree}</div>
+                <span>{group.school}</span>{" "}
+                <span>{getDateString(group.from, group.to)}</span>
+                <div>
+                  <Paragraphs string={group.details} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
 
-      <hr />
+      {work.length > 0 && (
+        <>
+          <hr />
 
-      <div>
-        <div>Work experience</div>
-
-        {work.map((group) => (
-          <div key={group.uniqueId}>
-            <div>{group.company}</div>
-            <span>{group.position}</span>{" "}
-            <span>{getDateString(group.from, group.to)}</span>
-            <div>
-              <Paragraphs string={group.details} />
-            </div>
+          <div>
+            <div>Work experience</div>
+            {work.map((group) => (
+              <div key={group.uniqueId}>
+                <div>{group.company}</div>
+                <span>{group.position}</span>{" "}
+                <span>{getDateString(group.from, group.to)}</span>
+                <div>
+                  <Paragraphs string={group.details} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 }
