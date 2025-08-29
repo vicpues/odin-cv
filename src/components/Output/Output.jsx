3140f16752d04b9ba92f4f71b3facad0e88bdd64
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-29 20:33:09 +0200

    feat: Style navbar and solve layout jank
    Basically, adding the navbar made everything sucky. Now it doesn't suck!

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -8,53 +8,55 @@ export default function Output({ data }) {
   const work = data.workData;
 
   return (
-    <output id="output-container">
-      <div className="output-page">
-        <div>
-          <div>{personal.name}</div>
-          <span>{personal.email}</span> <span>{personal.phone}</span>{" "}
-          <span>{personal.location}</span>
+    <div id="output-column">
+      <output id="output-container">
+        <div className="output-page">
           <div>
-            <Paragraphs string={personal.about} />
+            <div>{personal.name}</div>
+            <span>{personal.email}</span> <span>{personal.phone}</span>{" "}
+            <span>{personal.location}</span>
+            <div>
+              <Paragraphs string={personal.about} />
+            </div>
           </div>
+          {education.length > 0 && (
+            <>
+              <hr />
+              <div>
+                <div>Education</div>
+                {education.map((group) => (
+                  <div key={group.uniqueId}>
+                    <div>{group.degree}</div>
+                    <span>{group.school}</span>{" "}
+                    <span>{getDateString(group.from, group.to)}</span>
+                    <div>
+                      <Paragraphs string={group.details} />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+          {work.length > 0 && (
+            <>
+              <hr />
+              <div>
+                <div>Work experience</div>
+                {work.map((group) => (
+                  <div key={group.uniqueId}>
+                    <div>{group.company}</div>
+                    <span>{group.position}</span>{" "}
+                    <span>{getDateString(group.from, group.to)}</span>
+                    <div>
+                      <Paragraphs string={group.details} />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
-        {education.length > 0 && (
-          <>
-            <hr />
-            <div>
-              <div>Education</div>
-              {education.map((group) => (
-                <div key={group.uniqueId}>
-                  <div>{group.degree}</div>
-                  <span>{group.school}</span>{" "}
-                  <span>{getDateString(group.from, group.to)}</span>
-                  <div>
-                    <Paragraphs string={group.details} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        )}
-        {work.length > 0 && (
-          <>
-            <hr />
-            <div>
-              <div>Work experience</div>
-              {work.map((group) => (
-                <div key={group.uniqueId}>
-                  <div>{group.company}</div>
-                  <span>{group.position}</span>{" "}
-                  <span>{getDateString(group.from, group.to)}</span>
-                  <div>
-                    <Paragraphs string={group.details} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    </output>
+      </output>
+    </div>
   );
 }
