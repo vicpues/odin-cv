b96e2fbf3552d53e0e6db57b3ce69bcda4ad3ed3
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-09-03 16:11:52 +0200

    chore: Remove unused image library

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import { setupPlugins } from "@responsive-image/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr(),
-        setupPlugins({
-            include: /^[^?]+\.(jpg|webp|avif|png)\?.*responsive.*$/,
-            quality: 90,
-            format: ["jpg", "webp"],
-            lqip: { type: "inline" },
-        }),
-    ],
+    plugins: [react(), svgr()],
 });
