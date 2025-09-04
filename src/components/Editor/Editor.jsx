1383f07dd7716bb7e16d6da37eeb5ce57f158709
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-09-04 21:19:22 +0200

    feat: Add view state to editor

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+
 export default function Editor({ children }) {
-  return <main id="editor">{children}</main>;
+  const PARALLEL = "parallel";
+  const EDIT = "edit";
+  const PREVIEW = "preview";
+
+  const [view, setView] = useState(isMobile() ? EDIT : PARALLEL);
+
+  window.addEventListener("resize", () => {
+    setView(isMobile() ? EDIT : PARALLEL);
+  });
+
+  function isMobile() {
+    const MOBILE_BREAKPOINT = 600;
+    return window.innerWidth <= MOBILE_BREAKPOINT;
+  }
+
+  function swipeHandler(e) {
+    const editor = e.target;
+    const midpoint = editor.offsetWidth / 2;
+    const currentOffset = editor.scrollLeft;
+    setView(currentOffset < midpoint ? EDIT : PREVIEW);
+  }
+
+  return (
+    <main id="editor" onScroll={swipeHandler}>
+      {children}
+    </main>
+  );
 }
